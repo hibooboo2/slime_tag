802fe276495d825ebae4d88ab744887442fecb33
jjj932 <james.jeffrey1@ftr.com>
2025-02-15 17:11:45 -0700

    move power up stuff and fix collision bug

--- main.go
+++ main.go
@@ -360,59 +360,6 @@ func (e *Enemy) RandomMovement(playerX, playerY float64) {
 	}
 }
 
-type PowerUp struct {
-	icon      *AnimatedSprite
-	x, y      float64
-	spawnTime time.Time
-	bonus     int
-	collided  bool
-}
-
-func (pu *PowerUp) getRect() image.Rectangle {
-	return image.Rect(
-		int(pu.x)-20, int(pu.y)-20,
-		int(pu.x)+20, int(pu.y)+20,
-	)
-}
-
-func (pu *PowerUp) Overlaps(r image.Rectangle, game ecs.Game) bool {
-	g := game.(*Game)
-
-	pu.collided = pu.getRect().Overlaps(r)
-
-	if !pu.collided {
-		return false
-	}
-
-	switch pu.bonus {
-	case 0:
-		g.player.hpBar.AddHP(20)
-		g.AddFloatingText("+20 HP", float64(pu.x), float64(pu.y), color.RGBA{0, 255, 0, 255})
-	case 1:
-		g.player.hpBar.AddHP(-10)
-		g.AddFloatingText("-10 HP", float64(pu.x), float64(pu.y), color.RGBA{255, 0, 0, 255})
-	}
-
-	return true
-}
-
-func (pu *PowerUp) Draw(screen *ebiten.Image, game ecs.Game) {
-	g := game.(*Game)
-	// Draw the power-up icon at its location
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(pu.x, pu.y)
-	_, img := pu.icon.GetCurrentSprite(g.frameCount, 0)
-	screen.DrawImage(img, op)
-
-}
-
-func (pu *PowerUp) Remove() bool {
-	return time.Since(pu.spawnTime) > time.Second*10
-}
-
-var _ ecs.Drawer = &PowerUp{}
-var _ ecs.Overlapper = &PowerUp{}
-
 type FloatingText struct {
 	text      string
 	x, y      float64
--- powerup.go
+++ powerup.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
 	"math/rand"
 	"time"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hibooboo2/slime_tag/ecs"
 )
 
 func NewRandomPowerUp() *PowerUp {
@@ -20,4 +25,60 @@ func NewRandomPowerUp() *PowerUp {
 		p.icon = NewSprite(fmt.Sprintf("resources/slimes/PNG/%[1]s/Attack/%[1]s_Attack_full.png", "Slime3"), 10)
 	}
 	return p
+}
+
+type PowerUp struct {
+	icon      *AnimatedSprite
+	x, y      float64
+	spawnTime time.Time
+	bonus     int
+	collided  bool
+}
+
+var _ ecs.Drawer = &PowerUp{}
+var _ ecs.Overlapper = &PowerUp{}
+
+func (pu *PowerUp) getRect() image.Rectangle {
+	return image.Rect(
+		int(pu.x)-20, int(pu.y)-20,
+		int(pu.x)+20, int(pu.y)+20,
+	)
+}
+
+func (pu *PowerUp) Overlaps(r image.Rectangle, game ecs.Game) bool {
+	g := game.(*Game)
+	if pu.collided {
+		return true
+	}
+
+	pu.collided = pu.getRect().Overlaps(r)
+
+	if !pu.collided {
+		return false
+	}
+
+	switch pu.bonus {
+	case 0:
+		g.player.hpBar.AddHP(20)
+		g.AddFloatingText("+20 HP", float64(pu.x), float64(pu.y), color.RGBA{0, 255, 0, 255})
+	case 1:
+		g.player.hpBar.AddHP(-10)
+		g.AddFloatingText("-10 HP", float64(pu.x), float64(pu.y), color.RGBA{255, 0, 0, 255})
+	}
+
+	return true
+}
+
+func (pu *PowerUp) Draw(screen *ebiten.Image, game ecs.Game) {
+	g := game.(*Game)
+	// Draw the power-up icon at its location
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(pu.x, pu.y)
+	_, img := pu.icon.GetCurrentSprite(g.frameCount, 0)
+	screen.DrawImage(img, op)
+
+}
+
+func (pu *PowerUp) Remove() bool {
+	return time.Since(pu.spawnTime) > time.Second*10 || pu.collided
 }
